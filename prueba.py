f97472b3373a278d246d7041beeabe4bcd2bae47
tecnologiasqs-cmd <tecnologiasqs@gmail.com>
2026-05-01 14:35:05 -0500

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -3,3 +3,8 @@ import streamlit as st
 st.title(" Gente. Esta es la pagina de emprendedores")
 
 st.write("Esto es una prueba para utilizar el write")
+st.write("Vamos a utilizar algunas funciones del modulo streamlit")
+
+nombre = st.text_input("Ingresa tu nombre o alias?")
+
+st.write(nombre)
